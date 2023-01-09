da7f8fa6051efa3212e4395728df5220bfc6ee50
wise_JG <remit_@naver.com>
2023-01-09 21:44:31 +0900

    FirebaseChat
    클론

---
+++ FirebaseChat/app/src/main/java/com/firehong/firebasechat/model/User.kt
@@ -0,0 +1,16 @@
+package com.firehong.firebasechat.model
+
+class User {
+
+    var id: String = ""
+    var password: String = ""
+    var name: String = ""
+
+    constructor()
+
+    constructor(id:String, password:String, name:String) {
+        this.id = id
+        this.password = password
+        this.name = name
+    }
+}
