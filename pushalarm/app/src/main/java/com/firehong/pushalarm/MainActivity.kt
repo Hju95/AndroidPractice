6f71ce87d35ef2f3a89c5a787722bcd72713a9cb
wise_JG <remit_@naver.com>
2023-02-19 19:23:55 +0900

    json제외
    .

--- pushalarm/app/src/main/java/com/firehong/pushalarm/MainActivity.kt
+++ pushalarm/app/src/main/java/com/firehong/pushalarm/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.firehong.pushalarm
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
@@ -15,15 +17,20 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.firebaseTokenTextView)
     }
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         initFirebase()
-
+        updateResult()
     }
 
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+
+        setIntent(intent)
+        updateResult(true)
+    }
 
     private fun initFirebase() {
         FirebaseMessaging.getInstance().token
@@ -32,5 +39,15 @@ class MainActivity : AppCompatActivity() {
                     firebaseToken.text = task.result
                 }
             }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun updateResult(isNewIntent: Boolean = false) {
+        resultTextView.text = (intent.getStringExtra("notificationType") ?: "앱 런처") +
+                if (isNewIntent) {
+                    "(으)로 갱신했습니다."
+                } else {
+                    "(으)로 실행했습니다."
+                }
     }
 }
